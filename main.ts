1aa0d34bfc152c1a76b9452950822abe224bc331
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-09-29 17:06:54 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,19 @@
+let direction = 0
 basic.pause(200)
+basic.showIcon(IconNames.Happy)
+input.calibrateCompass()
 basic.forever(function () {
-	
+    direction = input.compassHeading()
+    if (direction > 260 && direction < 280) {
+        basic.showString("W")
+    }
+    if (direction > 170 && direction < 190) {
+        basic.showString("S")
+    }
+    if (direction > 80 && direction < 100) {
+        basic.showString("O")
+    }
+    if (direction > 0 && direction < 10) {
+        basic.showString("N")
+    }
 })
